3d035a3300ac531a34f03d4dd7d6512c22d8893a
VladTopor <vlad.toporkov08@gmail.com>
2023-12-31 12:15:37 +0200

    0.0.4

---
+++ src/ItemStack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vladislav on 11.12.23.
+//
+
+#include "ItemStack.h"
---
+++ src/ItemStack.h
@@ -0,0 +1,14 @@
+//
+// Created by vladislav on 11.12.23.
+//
+
+#ifndef PERPG_ITEMSTACK_H
+#define PERPG_ITEMSTACK_H
+
+
+class ItemStack {
+
+};
+
+
+#endif //PERPG_ITEMSTACK_H
